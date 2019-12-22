bb38fc4e38114fa4b347ae70ae456809d590d753
MildHat <work.mildhat@gmail.com>
2019-12-22 21:38:28 +0200

    Add getTableName() method

--- core/base/Model.php
+++ core/base/Model.php
@@ -12,10 +12,24 @@ abstract class Model
     public $errors = [];
     public $rules = [];
     public $db;
+    public $table = null;
 
     public function __construct()
     {
         $db = Db::instance();
         $this->db = $db->connection;
+    }
+
+    public function getTableName(): string
+    {
+        if ($this->table !== null && is_string($this->table)) {
+            return $this->table;
+        } else {
+            $className = get_class($this);
+            $className = explode('\\', $className);
+            $className = $className[array_key_last($className)];
+            $tableName = lcfirst($className) . 's';
+            return $tableName;
+        }
     }
 }
