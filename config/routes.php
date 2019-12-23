ffdd6baf471fb95d1bd240d616cdc21a4bfdf43a
MildHat <work.mildhat@gmail.com>
2019-12-23 16:36:39 +0200

    Add routes for Car

--- config/routes.php
+++ config/routes.php
@@ -2,8 +2,9 @@
 
 use \core\Router;
 
-Router::add('^page/(?P<id>[0-9]+)$', ['controller' => 'page', 'action' => 'show']);
-
+Router::add('^car/add$', ['controller' => 'car', 'action' => 'add']);
+Router::add('^car/(?P<id>[0-9]+)$', ['controller' => 'car', 'action' => 'show']);
+Router::add('^cars$', ['controller' => 'car', 'action' => 'index']);
 
 // default routes
 Router::add('^admin$', ['controller' => 'main', 'action' => 'index', 'prefix' => 'admin']);
