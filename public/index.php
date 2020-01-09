5aa522f0229c733834716fbc3a72023bdfea8fd4
MildHat <work.mildhat@gmail.com>
2020-01-09 19:39:30 +0200

    delete files with php in template files

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require dirname(__DIR__) . '/vendor/autoload.php';
-    require dirname(__DIR__) . '/config/init.php';
-    require dirname(__DIR__) . '/config/routes.php';
-
-    new core\App();
-
