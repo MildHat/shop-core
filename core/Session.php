bcc2cc6b7bf28f5bb461c5179d41fc4503046bc0
mildhat <mildhat@MacBook-Pro-mildhat.local>
2020-02-13 14:09:18 +0200

    Refactor Session class

--- core/Session.php
+++ core/Session.php
@@ -8,98 +8,166 @@ class Session
 {
     use TSingletone;
 
-    private $name = null;
-    private $savePath = null;
     /**
-     * @return mixed
-     * @throws \Exception
+     * Name of session
+     * @var string
      */
-    public function setName($name) {
+    private string $name = '';
+
+    /**
+     * Session save path
+     * @var string
+     */
+    private string $savePath = '';
+
+    /**
+     * Sets Session name
+     *
+     * @param string $name
+     * @return bool
+     */
+    public function setName(string $name) : bool
+    {
         if($this->sessionExists())
-            throw new \Exception('Cannot set session name. Session already started');
+            return false;
+
         $this->name = $name;
         session_name($name);
+        return true;
     }
+
     /**
+     * Returns Session name
+     *
      * @return mixed
      */
     public function getName() {
         return $this->name;
     }
+
     /**
-     * @param mixed $savePath
-     * @throws \Exception
+     * Sets Session save path
+     *
+     * @param string $savePath
+     * @return bool
      */
-    public function setSavePath($savePath) {
+    public function setSavePath(string $savePath) : bool
+    {
         if($this->sessionExists())
-            throw new \Exception('Cannot set session name. Session already started');
+            return false;
+
         $this->savePath = $savePath;
         session_save_path($savePath);
+        return true;
     }
+
     /**
+     * Returns Session save path
+     *
      * @return mixed
      */
     public function getSavePath()
     {
         return $this->savePath;
     }
-    public function set($key, $value)
+
+    /**
+     * Sets value by given key
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return bool
+     */
+    public function set(string $key, $value) : bool
     {
         if($this->sessionExists()) {
             $_SESSION[$key] = $value;
             return true;
         }
-        throw new \Exception('Cannot set value. Session is not started');
+
+        return false;
     }
-    public function get($key)
+
+    /**
+     * Returns value by the given key
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get(string $key)
     {
         if($this->sessionExists())
             return $_SESSION[$key];
-        throw new \Exception('Cannot get value. Session is not started');
+
+        return false;
     }
-    public function contains($key)
+
+    /**
+     * Checks if a value exists by given key
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function contains(string $key) : bool
     {
-        if($this->sessionExists()) {
-            if(isset($_SESSION)) {
-                if (array_key_exists($key, $_SESSION))
-                    return true;
-                return false;
-            }
-            return false;
-        }
-        throw new \Exception('Cannot check value. Session is not started');
-    }
-    public function delete($key)
-    {
-        if($this->sessionExists()) {
-            if (array_key_exists($key, $_SESSION)) {
-                unset($_SESSION[$key]);
+        if($this->sessionExists())
+            if (array_key_exists($key, $_SESSION))
                 return true;
-            }
-            return false;
-        }
-        throw new \Exception('Cannot delete value. Session is not started');
+
+        return false;
     }
-    public function start()
+
+    /**
+     * Delete value from the session by the given key
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function delete(string $key) : bool
+    {
+        if($this->sessionExists() && $this->contains($key)) {
+            unset($_SESSION[$key]);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Start the session
+     *
+     * @return bool
+     */
+    public function start() : bool
     {
         if(!$this->sessionExists()) {
             session_start();
             return true;
         }
-        throw new \Exception('Session is already started');
+
+        return false;
     }
-    public function cookieExists()
+
+    /**
+     * Checks if exists session cookie
+     *
+     * @return bool
+     */
+    public function cookieExists() : bool
     {
         if (isset($_COOKIE['PHPSESSID']))
             return true;
+
         return false;
     }
-    public function sessionExists()
+
+    /**
+     * Checks if session exists
+     *
+     * @return bool
+     */
+    public function sessionExists() : bool
     {
-        if (session_status() == PHP_SESSION_NONE) {
-            return false;
-        } elseif (session_status() == PHP_SESSION_ACTIVE) {
-            return true;
-        }
+        return session_status() === PHP_SESSION_ACTIVE ? true : false;
     }
 }
